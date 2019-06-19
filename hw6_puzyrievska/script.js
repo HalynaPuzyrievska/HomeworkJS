a49d1e0e0251f0b01e2954b2b468d60478e0151f
Halyna Puzyrievska <h.puzyrievska@gmail.com>
2019-06-19 10:39:00 +0300

    isNan

--- hw6_puzyrievska/script.js
+++ hw6_puzyrievska/script.js
@@ -4,22 +4,27 @@ var max=+prompt("how many characters do you want to get","max");
 var elCount=prompt("How many strings do you want to get?","");
 var words =[], normWords={};
 
-for (var i=elCount;i>0;--i) {
-var newWorld = "";
-var lettersCount = Math.floor(Math.random() * (max-min)+min);
+if (isNaN(max)||isNaN(min)||isNaN(elCount)) {
+    console.log("error");
+} else {
+    for (var i=elCount;i>0;--i) {
+        var newWorld = "";
+        var lettersCount = Math.floor(Math.random() * (max-min)+min);
+        
+            for (var j=lettersCount;j>0;--j){
+                var LettersIndex =Math.floor(Math.random()*(alphabet.length-1));
+                newWorld+=alphabet[LettersIndex];
+            }
+            if (!normWords[newWorld.length]) {
+                normWords[newWorld.length] = [];
+            } else {
+            }   normWords[newWorld.length].push(newWorld);
+        }
+        console.log(normWords);
+        console.log(elCount + " new word: ");
+        for (key in normWords) {
+            var result = normWords[key].length;
+            console.log(result + " word with " + key + " characters ");
+        }
+}
 
-    for (var j=lettersCount;j>0;--j){
-        var LettersIndex =Math.floor(Math.random()*(alphabet.length-1));
-        newWorld+=alphabet[LettersIndex];
-    }
-    if (!normWords[newWorld.length]) {
-        normWords[newWorld.length] = [];
-    } else {
-    }   normWords[newWorld.length].push(newWorld);
-}
-console.log(normWords);
-console.log(elCount + " new word: ");
-for (key in normWords) {
-    var result = normWords[key].length;
-    console.log(result + " word with " + key + " characters ");
-}
