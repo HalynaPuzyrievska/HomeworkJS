a08196d155c889028dcf08c4973e90e70e42c5cf
Halyna Puzyrievska <h.puzyrievska@gmail.com>
2019-07-22 16:15:55 +0300

    change folder

--- hw10_puzyrievska/script.js
+++ hw10_puzyrievska/script.js
@@ -6,67 +6,67 @@
    this.maxDistance = 1000;
    this.image = image;
 
-};
+}
 
 MilitaryResource.prototype.isReadyToMove = function() {
     return this.maxDistance >  this.distance;
-};
+}
 
 MilitaryResource.prototype.isReadyToFight = function() {
    return this.health >  0;
-};
+}
 
 MilitaryResource.prototype.restore = function() {
    this.health = this.maxHealth;
    this.distance = 0;
-};
+}
 
 MilitaryResource.prototype.clone = function() {
     return new MilitaryResource(this.type, this.health, this.image, this.distance);
-};
+}
 
 function Squad (resources) {
     this.resources = resources && resources.length ?
         resources.map(function(resources){
             return resources;
         })  :  [];
-};
+}
 
 Squad.prototype.isReadyToMove = function(){
     return this.resources.every(function(resource) {
         return resource.isReadyToMove();
     })
-};
+}
 
 Squad.prototype.isReadyToFight = function()   {
     return this.resources.every(function(resource){
         return resource.isReadyToFight();
     })
-};
+}
 
 Squad.prototype.getReadyToMoveResources = function(){
     return this.resources.filter(function(resource) {
         return resource.isReadyToMove();
     })
-};
+}
 
 Squad.prototype.restore = function() {
     return this.resource.forEach(function(resource){
         return resource.restore();
     })
-};
+}
 
 Squad.prototype.combineResources = function(defaultResources){
     return this.defaultResources.sort(function() {
         return .5 - Math.random();
    });
-};
+}
 
 Squad.prototype.clone = function () {
     return new Squad (this.resources.map(function(resources){
         return resources.clone();
     }));
-};
+}
 
 var winni = new MilitaryResource("Land_Winni", 100, "images/winnie_pooh.png", 200);
 var tigger = new MilitaryResource("Air_Tigger", 50, "images/tigger.png", 90);
@@ -87,7 +87,7 @@ function loadCards() {
         document.getElementById('out').lastChild.innerHTML += '<div class="stamina indicator"></div>';
         document.getElementById('out').lastChild.innerHTML += '<h2 class="title_character">' + disney[key].type + '</h2>';
         document.getElementById('out').lastChild.innerHTML += '<div class="image"></div>';
-        document.getElementById('out').lastChild.innerHTML += '<img src="' + disney[key].image + '"class ="character" width="120" height="120" alt="">';
+        document.getElementsByClassName('image')[key].innerHTML += '<img class="character" src="' + disney[key].image + '"class ="character" width="120" height="120" alt="character"></img>'; 
     }
 }
 
